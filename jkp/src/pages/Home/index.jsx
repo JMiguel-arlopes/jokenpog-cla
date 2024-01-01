ef3acbc4c5411a9c4b500e54311f654f66c55c52
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-01 20:33:56 -0300

    inserindo página MLBB

--- jkp/src/pages/Home/index.jsx
+++ jkp/src/pages/Home/index.jsx
@@ -1,12 +1,10 @@
-import Navbar from "../../components/Navbar";
 import AboutUs from "../../layout/AboutUs";
 import Banner from "../../layout/Banner";
 import Plataform from "../../layout/Plataform";
 
 export default function Home() {
     return (
-        <>
-            <Navbar />
+        <>  
             <Banner/>
             <AboutUs/>
             <Plataform/>
---
+++ jkp/src/pages/Mlbb/index.jsx
@@ -0,0 +1,14 @@
+import Banner_mlbb from '../../layout/Banner_mlbb';
+import Extensions from '../../layout/Extensions';
+import SwipperHall from '../../layout/SwipperHall';
+import styles from './mlbb.module.css';
+
+export default function Mlbb() {
+    return (
+        <>
+            <Banner_mlbb />
+            <Extensions/>
+            <SwipperHall/>
+        </>
+    )
+}
