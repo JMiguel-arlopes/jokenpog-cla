213fe5c131824528952bb18c452c200438ed92d5
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-01 20:35:50 -0300

    inserindo novas atualizações

--- jkp/src/App.js
+++ jkp/src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
-import Home from './pages/Home';
+import "./App.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
   );
 }
