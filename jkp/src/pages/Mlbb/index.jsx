5ffcb69cb0bb7df218145c45feaba7d77270ccfe
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-04 23:11:01 -0300

    inserindo a section de rank

--- jkp/src/pages/Mlbb/index.jsx
+++ jkp/src/pages/Mlbb/index.jsx
@@ -1,14 +1,17 @@
 import Banner_mlbb from '../../layout/Banner_mlbb';
 import Extensions from '../../layout/Extensions';
+import Ranks from '../../layout/Ranks';
 import SwipperHall from '../../layout/SwipperHall';
+import db from '../../db.json';
 import styles from './mlbb.module.css';
 
 export default function Mlbb() {
     return (
         <>
             <Banner_mlbb />
-            <Extensions/>
-            <SwipperHall/>
+            <Extensions />
+            <SwipperHall db={db}/>
+            <Ranks db={db}/>
         </>
     )
 }
