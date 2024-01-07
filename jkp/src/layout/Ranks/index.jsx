019d420a7c737cad9861a2db375d25513570f33a
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-06 23:04:17 -0300

    inserindo novos calculos e funcionalidades para ranquamento

--- jkp/src/layout/Ranks/index.jsx
+++ jkp/src/layout/Ranks/index.jsx
@@ -1,34 +1,46 @@
 import styles from './ranks.module.css';
 import Container from '../../components/Container'
 import { useEffect, useState } from 'react';
+import CardSimpleStatistic from '../statistic/CardSimpleStatistic';
+import CardRankStatistic from '../statistic/CardRankStatistic';
 
 export default function Ranks(prop) {
 
     const {db} = prop;
     const members = db.members;
+    const allLanesPlayers = members.map(player => player.lane)
 
-    const [mediaWR, setMediaWR] = useState(0)
-    const [isLanes, setlanes] = useState([])
+    const [averageWR, setAverageWR] = useState(0)
+    const [totalMatches, setTotalMatches] = useState(0)
+    const [totalTitle, setTotalTitles] = useState(0)
+    const [rankLane, setRankLane] = useState([])
+    const [rankMedal, setRankMedal] = useState([])
+    
+    
 
     const calculateAverageWinRate = () => {
         
         const total = members.reduce((total, itemAtual) => total + itemAtual.winRate, 0)
 
-        const media = (total / members.length).toFixed(2)
-        setMediaWR(media)
+        const media = (total / members.length).toFixed(2) + '%'
+        setAverageWR(media)
     }
 
-    const calculateMostUsedLanes = () => {
-        const allLanesPlayers = members.map(player => player.lane)
-        const contagemLanes = {}
+    const sumValues = (key) => {
+        const result = members.reduce((acc, item) => acc + item[key], 0)
+        return result
+    }
+
+    const calculateMostUsedItemInArray = (arrayToCount) => {
+        const contagem = {}
         
         // aqui nós vamos pegar as lanes de todos no banco de dados
         // usamos o forEach para percorrer por todas as lanes
         // pegamos o objeto "contagemLanes" e criamos uma key baseado no item(lane) da array do banco de dados
         // se o item já estiver "registrada" e dentro do "contagem", soma 1
         // caso ainda não tenha, como ocorre inicialmente, ele apenas cria essa key e põe valor como 0
-        allLanesPlayers.forEach(lanePlayer => {
-            contagemLanes[lanePlayer] = (contagemLanes[lanePlayer] || 0) + 1;
+        arrayToCount.forEach(itemArray => {
+            contagem[itemArray] = (contagem[itemArray] || 0) + 1;
         })
 
         // Aqui foi usado algo bem diferente.
@@ -36,45 +48,47 @@ export default function Ranks(prop) {
         // obs: como o nome sugere, essa array contém somente as chaves do item
         // aí fizemos um map com essa nova array e esse map transformamos a array anterior em uma array de objetos
         // cada objeto tem um nameLane e a quantidade e com isso conseguimos agora imprimir na tela
-        const laneList = Object.keys(contagemLanes);
-        const result = laneList.map(lane => (
-            {nameLane: lane, quantidade: contagemLanes[lane]}
+        const ListKeys = Object.keys(contagem);
+        const result = ListKeys.map(key => (
+            {name: key, value: contagem[key]}
         ))
 
         // aqui nós somente ordenamos a lista para ela ficar na ordem da maior para menor
-        const listaOrdenada = result.sort((a, b) => b.quantidade - a.quantidade)
-        setlanes(listaOrdenada)
+        return result.sort((a, b) => b.value - a.value)
     }
 
+    const sortPlayersBy = (key) => {
+        const selection = members.map(player => (
+            {name: player.name, value: player[key]}
+        ))
+        
+            
+        const ranking = selection.filter(item => item.value > 0)
+        const shuffle = ranking.sort(() => Math.random() - 0.5);
+        return shuffle.sort((a, b) => b.value - a.value)
+    }
+
+
     useEffect(() => {
+        setRankLane(calculateMostUsedItemInArray(allLanesPlayers))
+        setRankMedal(sortPlayersBy('medal'))
+        setTotalMatches(sumValues('matches'))
+        setTotalTitles(sumValues('titles'))
+        
         calculateAverageWinRate()
-        calculateMostUsedLanes()
     }, [members])
     
-    
     return (
-        <section>
-            <Container>
-                <div>
-                    <h2>Média WinRate do clã</h2>
-                    <p>{mediaWR}</p>
-                </div>
-                <div>
-                    <h2>rank de lanes mais usadas</h2>
-                    <div>
-                        <h4>{}</h4>
-                        {isLanes.map((lane, index) => {
-                            return (
-                                <p>
-                                    {index+1}º {lane.nameLane}: {lane.quantidade}
-                                </p>
-                            )
-                        })}
-                    </div>
-                </div>
-                <div>
-                    <h2>Terceiro</h2>
-                </div>
+        <section className={styles.rank_container}>
+            <Container modifier='stretch'>
+                <CardSimpleStatistic title='Partidas totais' value={totalMatches}/>
+                <CardSimpleStatistic title='Win Rate Geral' value={averageWR}/>
+                <CardSimpleStatistic title='Titulos Nacionais' value={totalTitle}/>
+                {/* <CardSimpleStatistic title='MCL' value={totalMatches}/> */}
+            </Container>
+            <Container modifier='stretch'>
+                <CardRankStatistic rank={rankLane} title={'lanes'}/>
+                <CardRankStatistic rank={rankMedal} title={'Campeões JKP'}/>
             </Container>
         </section>
     )
