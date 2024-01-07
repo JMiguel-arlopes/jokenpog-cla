396eb1ba457054187218cb8579b58dad6d2b0439
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-07 12:36:28 -0300

    melhorando estatisticas

--- jkp/src/layout/statistic/ListRank/index.jsx
+++ jkp/src/layout/statistic/ListRank/index.jsx
@@ -5,17 +5,46 @@ export default function ListRank(props) {
     const {rank} = props;
 
     return (
-        <ol className={styles.list}>
+        // <ol className={styles.list}>
+        <table className={styles.table}>
+            <tr className={styles.header}>
+                <th>Classificação</th>
+                <th>Nome</th>
+                <th>Medalhas</th>
+            </tr>
             {rank.map((item, index) => {
+                const firstItem = index === 0
+                const lastItem = index === rank.length - 1
+
+
                 return (
-                    <li className={styles.item}>
-                        <span>{index+1}°</span>
-                        <h4>{item.name}</h4>
-                        <h5>{item.value}</h5>
-                    </li>
+                    <tr className={`
+                        ${firstItem ? styles.first : ''}
+                        ${lastItem ? styles.last : ''}
+                    `}>
+                        <td className={styles.classification}>{index+1}º</td>
+                        <td className={styles.name}>{item.name}</td>
+                        <td className={styles.point}>{item.value}</td>
+                    </tr>
+                    
+
+
+
+                    // <li 
+                    //     className={`
+                    //         ${styles.item}
+                    //         ${firstItem ? styles.first : ''}
+                    //         ${lastItem ? styles.last : ''}
+                    //     `}
+                    // >
+                    //     <span>{index+1}°</span>
+                    //     <h4>{item.name}</h4>
+                    //     <h5>{item.value}</h5>
+                    // </li>
                 )
             })}
-        </ol>
+        </table>
+        // </ol>
         
     )
 }
