48105e9c99a190ae08758435d1e1d872f8da1186
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-10 20:12:10 -0300

    mudando arquitetura do projeto

--- jkp/src/pages/Mlbb/index.jsx
+++ jkp/src/pages/Mlbb/index.jsx
@@ -1,17 +1,16 @@
 import Banner_mlbb from '../../layout/Banner_mlbb';
 import Extensions from '../../layout/Extensions';
 import Ranks from '../../layout/Ranks';
-import SwipperHall from '../../layout/SwipperHall';
 import db from '../../db.json';
-import styles from './mlbb.module.css';
+import AboutUs from '../../layout/AboutUs';
 
 export default function Mlbb() {
     return (
         <>
             <Banner_mlbb />
-            <Extensions />
-            <SwipperHall db={db}/>
+            <AboutUs db={db}/>
             <Ranks db={db}/>
+            <Extensions />
         </>
     )
 }
