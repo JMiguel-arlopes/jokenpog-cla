5558651053a794214ddfccc1821d00643dd3ba13
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-10 20:15:50 -0300

    inserindo neon

--- jkp/src/layout/statistic/CardSimpleStatistic/index.jsx
+++ jkp/src/layout/statistic/CardSimpleStatistic/index.jsx
@@ -2,7 +2,7 @@ import styles from './cardsimplestatistic.module.css';
 
 export default function CardSimpleStatistic(props) {
     
-    const {title, value} = props
+    const {title, value, suffix} = props
     
     return (
         <div className={styles.container_card_simple}>
@@ -10,7 +10,7 @@ export default function CardSimpleStatistic(props) {
                 <h2>{title}</h2>
             </div>
             <div className={styles.content_percent}>
-                <p>{value}</p>
+                <p>{value}{suffix}</p>
             </div>
             
         </div>
