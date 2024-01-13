4d26e1a1a52246a592082345c388e97bf80cadae
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-13 07:56:56 -0300

    mudando variaveis de cores nas sections

--- jkp/src/sections/Banner/index.jsx
+++ jkp/src/sections/Banner/index.jsx
@@ -3,17 +3,16 @@ import Container from '../../components/Container';
 import bg from '../../assets/gif-bg.gif'
 import Writer from '../../components/Writer';
 import { FaYoutube, FaDiscord } from "react-icons/fa";
-import { IoMdDisc } from "react-icons/io";
-
+import logo from '../../assets/jkp-logo-semFundo.png';
 
 export default function Banner_mlbb() {
     return (
         <section className={styles.container_banner_mlbb}>
             <div className={styles.information_banner}>
-                <Container modifier='start'>
-                    <IoMdDisc className={styles.disc}/>
-                    <h3>JokenpoG Team</h3>
-                </Container>
+                <figure>
+                    <img src={logo} alt='logotipo'/>
+                    <figcaption>JokenpoG Team</figcaption>
+                </figure>
                     
                 <div className={styles.content_banner}>
                     <h3>primeiro</h3>
@@ -34,9 +33,9 @@ export default function Banner_mlbb() {
                 </div>       
             </div>
 
-            <Container flex={1.75} modifier='center'>
+            <div className={styles.banner_img}>
                 <img src={bg} alt="imagem aleatória" />
-            </Container>
+            </div>
             
             
         </section>
