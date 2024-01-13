cf9056be3c5e877fa25e883ea3e2d41da07af45a
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-13 06:47:19 -0300

    inserindo rolagem, hover e espaçamento

--- jkp/src/components/Navbar/index.jsx
+++ jkp/src/components/Navbar/index.jsx
@@ -3,8 +3,10 @@ import logo from '../../assets/logo.jpg'
 import { useState } from 'react'
 import { IoMenu } from "react-icons/io5";
 import { FaX } from "react-icons/fa6";
+import { Link, animateScroll as scroll } from 'react-scroll';
 
 export default function Navbar() {
+    
 
     const [actived, setActived] = useState(false)
 
@@ -33,18 +35,36 @@ export default function Navbar() {
             }
 
             <div className={`${styles.links} ${styles.show}`}>
-                <a href="/">Sobre nós</a>
-                <a href="/">Membros</a>
-                <a href="/">Estatisticas</a>
-                <a href="/">Funcionalidades</a>
+                <div className={`${styles.links} ${styles.hide}`}>
+                    <Link to="aboutus" smooth={true} duration={800} spy={true}>
+                        Sobre nós
+                    </Link>
+                    <Link to="members" smooth={true} duration={800} spy={true}>
+                        Membros
+                    </Link>
+                    <Link to="statistics" smooth={true} duration={800} spy={true}>
+                        Estatísticas
+                    </Link>
+                    <Link to="functionalities" smooth={true} duration={800} spy={true}>
+                        Funcionalidades
+                    </Link>
+                </div>
             </div>
 
             {actived && (
                 <div className={`${styles.links} ${styles.hide}`}>
-                    <a href="/">Sobre nós</a>
-                    <a href="/">Membros</a>
-                    <a href="/">Estatisticas</a>
-                    <a href="/">Funcionalidades</a>
+                    <Link to="aboutus" smooth={true} duration={500} spy={true}>
+                        Sobre nós
+                    </Link>
+                    <Link to="members" smooth={true} duration={500} spy={true}>
+                        Membros
+                    </Link>
+                    <Link to="statistics" smooth={true} duration={500} spy={true}>
+                        Estatísticas
+                    </Link>
+                    <Link to="functionalities" smooth={true} duration={500} spy={true}>
+                        Funcionalidades
+                    </Link>
                 </div>
             )}
             
