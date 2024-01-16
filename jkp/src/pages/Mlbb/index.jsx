4a67e63b73d86d7c789e77a3adf8136822bceb85
JM-arlopes <jmiguelarlopes@gmail.com>
2024-01-15 23:52:04 -0300

    mudando rotina para o mlbb

--- jkp/src/pages/Mlbb/index.jsx
+++ jkp/src/pages/Mlbb/index.jsx
@@ -12,6 +12,51 @@ export default function Mlbb() {
         return result
     }
 
+    const calculateAverage = (key) => {
+        const total = sumValues(key)
+
+        const media = (total / members.length).toFixed(2)
+        return media
+    }
+
+    const calculateMostUsedItemInArray = (key) => {
+        const arrayToCount = members.map(member => member[key])
+        const contagem = {}
+        
+        // aqui nós vamos pegar as lanes de todos no banco de dados
+        // usamos o forEach para percorrer por todas as lanes
+        // pegamos o objeto "contagemLanes" e criamos uma key baseado no item(lane) da array do banco de dados
+        // se o item já estiver "registrada" e dentro do "contagem", soma 1
+        // caso ainda não tenha, como ocorre inicialmente, ele apenas cria essa key e põe valor como 0
+        arrayToCount.forEach(itemArray => {
+            contagem[itemArray] = (contagem[itemArray] || 0) + 1;
+        })
+
+        // Aqui foi usado algo bem diferente.
+        // pegamos aquele objeto e transformamos em uma array por meio do "Object.keys"
+        // obs: como o nome sugere, essa array contém somente as chaves do item
+        // aí fizemos um map com essa nova array e esse map transformamos a array anterior em uma array de objetos
+        // cada objeto tem um nameLane e a quantidade e com isso conseguimos agora imprimir na tela
+        const ListKeys = Object.keys(contagem);
+        const result = ListKeys.map(key => (
+            {name: key, value: contagem[key]}
+        ))
+
+        // aqui nós somente ordenamos a lista para ela ficar na ordem da maior para menor
+        return result.sort((a, b) => b.value - a.value)
+    }
+
+    const sortPlayersBy = (key) => {
+        const selection = members.map(player => (
+            {name: player.name, value: player[key]}
+        ))
+        
+            
+        const ranking = selection.filter(item => item.value > 0)
+        const shuffle = ranking.sort(() => Math.random() - 0.5);
+        return shuffle.sort((a, b) => b.value - a.value)
+    }
+
     return (
         <>
             <Banner_mlbb />
@@ -21,7 +66,15 @@ export default function Mlbb() {
                 amountMCL={sumValues('mcl')}
                 amountMatches={sumValues('matches')}
             />
-            <Statistics db={db}/>
+            <Statistics
+                amountMatches={sumValues('matches')}
+                amountTitles={sumValues('titles')}
+                averageWR={calculateAverage('winRate')}
+                averageMCL={calculateAverage('mcl')}
+                rankingLane={calculateMostUsedItemInArray('lane')}
+                rankingElo={calculateMostUsedItemInArray('elo')}
+                rankingMedal={sortPlayersBy('medal')}
+            />
             <Extensions />
         </>
     )
